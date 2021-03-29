5f4f1685312b819f6263f6648ba5e446da3281d0
Vladimir Kabanets <karolsrebrny@gmail.com>
2021-03-29 20:30:40 +0300

    Commit 1

---
+++ src/IpApiGeoService.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace Hillel\GeoServ1;
+
+
+use Illuminate\Support\Facades\Http;
+use Hillel\GeoInterface\GeoServiceInterface;
+
+class IpApiGeoService implements GeoServiceInterface
+{
+    protected $data;
+
+    public function parse($ip)
+    {
+        $response = Http::get('http://ip-api.com/json/'. $ip .'?fields=continentCode,countryCode');
+
+        $this->data = $response->json();
+    }
+    public function continentCode()
+    {
+        return $this ->data['continentCode'];
+    }
+
+    public function countryCode()
+    {
+     return $this->data['countryCode'];
+    }
+}
